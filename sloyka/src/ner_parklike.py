893039f6cc6d735a406c35cf3a60019377ed8d15
George <george.kontsevik@gmail.com>
2024-02-13 22:13:19 +0300

    fix/greening finder: small refactor and bugfix

--- sloyka/src/ner_parklike.py
+++ sloyka/src/ner_parklike.py
@@ -1,13 +1,24 @@
+from natasha.extractors import Extractor
+import natasha.obj as obj
+import pandas as pd
+from natasha.extractors import Match
+
+
+from .rule_for_natasha import ADDR_PART
+
 from natasha import (
     Segmenter,
     MorphVocab,
-    
     NewsEmbedding,
     NewsMorphTagger,
     NewsSyntaxParser,
     NewsNERTagger,
-    Doc
+    Doc,
 )
+
+from tqdm import tqdm
+
+tqdm.pandas()
 
 segmenter = Segmenter()
 morph_vocab = MorphVocab()
@@ -16,15 +27,10 @@ emb = NewsEmbedding()
 morph_tagger = NewsMorphTagger(emb)
 syntax_parser = NewsSyntaxParser(emb)
 ner_tagger = NewsNERTagger(emb)
-from natasha.extractors import Extractor
-import natasha.obj as obj
-import pandas as pd
-from natasha.extractors import Match
-import pdb
+
 
 class AddrNEWExtractor(Extractor):
     def __init__(self, morph):
-        from factfinder.src.addrNEW import ADDR_PART
         Extractor.__init__(self, ADDR_PART, morph)
 
     def find(self, text):
@@ -33,12 +39,13 @@ class AddrNEWExtractor(Extractor):
             return
 
         matches = sorted(matches, key=lambda _: _.start)
-        if not matches:  
+        if not matches:
             return
         start = matches[0].start
         stop = matches[-1].stop
         parts = [_.fact for _ in matches]
         return Match(start, stop, obj.Addr(parts))
+
 
 class NER_parklike:
     @staticmethod
@@ -49,29 +56,39 @@ class NER_parklike:
         morph = MorphVocab()
         extractor = AddrNEWExtractor(morph)
 
-        data = {'start': "", 'stop': "", 'toponim': "", 'toponim_type': ""}
+        data = {"start": None, "stop": None, "toponim": None, "toponim_type": None}
 
         match = extractor.find(text)
         if not match:
-            return pd.Series([data['start'], data['stop'], data['toponim'], data['toponim_type']])
+            return pd.Series(
+                [
+                    data["start"],
+                    data["stop"],
+                    data["toponim"],
+                    data["toponim_type"],
+                ]
+            )
+        found_toponim = match.fact.parts[0].value
+        if found_toponim:
+            data["start"] = match.start
+            data["stop"] = match.stop
+            data["toponim"] = found_toponim
+            data["toponim_type"] = match.fact.parts[0].type
 
-        data['start'] = match.start
-        data['stop'] = match.stop
-        data['toponim'] = match.fact.parts[0].value
-        data['toponim_type'] = match.fact.parts[0].type
-        return pd.Series([data['start'], data['stop'], data['toponim'], data['toponim_type']])
-        
+        return pd.Series(
+            [data["start"], data["stop"], data["toponim"], data["toponim_type"]]
+        )
 
     @staticmethod
     def natasha_normalization(text, start_ind, stop_ind):
         """
-        This function extracts entities and normalize them. For entities that match the 
+        This function extracts entities and normalize them. For entities that match the
         extraction of "extract_parklike", it returns a normalized form.
         """
         normalized_form = ""
         if start_ind != "":
             return normalized_form
-        
+
         doc = Doc(text)
         doc.segment(segmenter)
         doc.tag_morph(morph_tagger)
@@ -82,11 +99,21 @@ class NER_parklike:
                 span.normalize(morph_vocab)
                 normalized_form = span.normal
         return normalized_form
-    
-    def run(self,df, text_column):
-        df[["Start", "Stop", "Toponim", "Toponim_type"]] = df[text_column].apply(lambda text: NER_parklike.extract_parklike(text) if NER_parklike.extract_parklike(text) is not None else pd.Series(["", "", "", ""]))
-        df["Normal"] = df.apply(lambda row: NER_parklike.natasha_normalization(row[text_column], row["Start"], row["Stop"]),axis=1)
-        
 
-        
-                     
+    def run(self, df, text_column):
+        df[["Start", "Stop", "Toponim", "Toponim_type"]] = df[
+            text_column
+        ].progress_apply(
+            lambda text: (
+                NER_parklike.extract_parklike(text)
+                if NER_parklike.extract_parklike(text) is not None
+                else pd.Series(["", "", "", ""])
+            )
+        )
+        # df["Normal"] = df.progress_apply(
+        #     lambda row: NER_parklike.natasha_normalization(
+        #         row[text_column], row["Start"], row["Stop"]
+        #     ),
+        #     axis=1,
+        # )
+        return df
