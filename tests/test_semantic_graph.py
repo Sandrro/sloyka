b6d4ac002b4f512a5b52d0af4e3d4ab86fd564d1
George <george.kontsevik@gmail.com>
2024-02-13 18:34:31 +0300

    tests fix

--- tests/test_semantic_graph.py
+++ tests/test_semantic_graph.py
@@ -4,7 +4,7 @@ from sloyka import Semgraph
 
 
 sm = Semgraph()
-test_df  = pd.read_feather("I:\\sloyka\\data\\processed\\df_strts.feather")[:500]
+test_df  = pd.read_feather("sloyka/sample_data/processed/df_strts.feather")[:500]
 text_column='Текст комментария'
 toponim_column='only_full_street_name'
 toponim_name_column='initial_street'
