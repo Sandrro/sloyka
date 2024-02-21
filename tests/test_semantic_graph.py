1c5cfe6e96b6437ed2deabebebb1fa4a88985d5f
Leon Tur <tutrkov.leon-2@yandex.ru>
2024-02-21 13:27:17 +0300

    Fixed speeded up tests

--- tests/test_semantic_graph.py
+++ tests/test_semantic_graph.py
@@ -4,7 +4,7 @@ from sloyka import Semgraph
 
 
 sm = Semgraph()
-test_df  = pd.read_feather("sloyka/sample_data/processed/df_strts.feather")[:500]
+test_df  = pd.read_feather("sloyka/sample_data/processed/df_strts.feather")[:20]
 text_column='Текст комментария'
 toponim_column='only_full_street_name'
 toponim_name_column='initial_street'
@@ -16,10 +16,10 @@ def test_extract_keywords():
                         toponim_column,
                         toponim_name_column,
                         toponim_type_column,
-                        semantic_score_filter=0.6,
+                        semantic_key_filter=0.6,
                         top_n=5)
 
-    assert len(result) == 57
+    assert len(result) == 7
 
 def test_get_semantic_closeness():
     df = pd.DataFrame([['TOPONIM_1', 'роза'], ['TOPONIM_2', 'куст']], columns=['toponims', 'words'])
@@ -37,7 +37,8 @@ def test_build_semantic_graph():
                                      toponim_column,
                                      toponim_name_column,
                                      toponim_type_column,
+                                     key_score_filter=0.4,
                                      semantic_score_filter=0.6,
                                      top_n=5)
     
-    assert len(result.edges) == 387
+    assert len(result.edges) == 216
