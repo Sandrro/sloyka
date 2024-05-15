c78df7187cbcd36e3024682f69ccbcf88312ddd9
Vladimir Vorona <142047864+Voronapxl@users.noreply.github.com>
2024-05-15 20:03:19 +0300

    Update test_area_matcher.py
    removed VK API key

--- tests/test_area_matcher.py
+++ tests/test_area_matcher.py
@@ -8,7 +8,7 @@ from sloyka.src.data_getter import VKParser
 @pytest.fixture
 def test_df_groups():
     domain = 'pkio_klp'
-    access_token = '96cbbc1496cbbc1496cbbc14b795dfa8b8996cb96cbbc14f2a8294fa4c4c6fc7e753a93'
+    access_token = '...'
     df_groups = VKParser().run_parser(domain, access_token, step=100, cutoff_date='2024-03-20', number_of_messages=10)
     return df_groups
 
