f17c85f6c741a7c51f04f19fca42bae98a324c8e
leontur <turkov.leon-2@yandex.ru>
2024-06-02 00:34:35 +0300

    Created module and class for regional activity detection. Process functions created. Class approach is still controversial, may be revised.

--- sloyka/src/regional_activity.py
+++ sloyka/src/regional_activity.py
@@ -13,53 +13,77 @@ class RegionalActivity:
 
     def __init__(self,
                  data: pd.DataFrame,
+                 osm_id: int,
+                 tags: dict,
+                 date: str,
                  text_column: str = 'text',
                  group_name_column: str = 'group_name',
-                 group_region_column: str = 'territory',
-                 admin_level_column: str = 'admin_level'
+                 repository_id: str = 'Sandrro/text_to_subfunction_v10',
+                 number_of_categories: int = 1,
+                 device: str='cpu'
                  ) -> None:
-        """
-        This function initializes the RegionalActivity class by
-        Args:
-            group_region_column: The name of the region to be analyzed
-        """
+
+        self.geocoder = Geocoder()
         self.data = data
-        self.geodata = None
+        self.processed_geodata = None
+        self.osm_id = osm_id
+        self.tags = tags
+        self.date = date
         self.text = text_column
         self.group_name = group_name_column
-        self.group_region = group_region_column
-        self.admin_level = admin_level_column
+        self.text_classifier = TextClassifiers(repository_id=repository_id,
+                                               number_of_categories=number_of_categories,
+                                               device_type=device)
 
-    def geocode_data(self) -> gpd.GeoDataFrame:
+    def geocode_data(self) -> None:
         """
         This function geocodes the data and returns the comments with the points and addresses
         Returns:
-            gpd.GeoDataFrame
+            None
         """
-        geocoded_data = gpd.GeoDataFrame()
-        groups = self.data[self.group_name].unique()
 
-        for i in groups:
-            data_info = self.data.loc[self.data[self.group_name] == i]
+        geocoded_data = self.geocoder.run(df=self.data,
+                                          osm_id=self.osm_id,
+                                          tags=self.tags,
+                                          date=self.date,
+                                          text_column=self.text,
+                                          group_column=self.group_name)
 
-            city = data_info[self.group_region].iloc[0]
-            level = int(data_info[self.admin_level].iloc[0])
+        self.processed_geodata = geocoded_data
 
-            sloyka_geocoder = Geocoder(osm_city_name=city, osm_city_level=level)
+    def service_data(self) -> None:
+        """
+        This function extracts service from texts
+        Returns:
+            None
+        """
 
-            tmp_df = self.data.loc[self.data[self.group_region] == i]
-            tmp_gdf = sloyka_geocoder.run(tmp_df, text_column=self.text)
+        service_data = City_services().run(df=self.processed_geodata,
+                                           text_column=self.text)
 
-            geocoded_data = gpd.GeoDataFrame(pd.concat([geocoded_data, tmp_gdf]), index=geocoded_data.index)
+        self.processed_geodata = service_data
 
-            self.geodata = geocoded_data
+    def class_text(self) -> None:
+        """
+        This functions classifies texts according to extracted function
+        Returns:
+            None
+        """
 
-        return geocoded_data
+        self.processed_geodata[['classified_text', 'probs']] = (self.processed_geodata[self.text].progress_map(
+                                                                lambda x: self.text_classifier.run_text_classifier(x)).
+                                                                to_list())
 
 
 if __name__ == "__main__":
     df = pd.read_csv("C:\\Users\\thebe\\OneDrive - ITMO UNIVERSITY\\НИРМА\\Data\\test_data\\regional_activity\\messages.csv")
 
-    ra = RegionalActivity(data=df)
-    print(ra.data)
-    print(ra.geocode_data())
+    ra = RegionalActivity(data=df,
+                          osm_id=338635,
+                          tags={"admin_level": ["8"]},
+                          date="2024-04-22T00:00:00Z",
+                          text_column='text')
+
+    ra.geocode_data()
+    ra.service_data()
+    print(ra.processed_geodata)
