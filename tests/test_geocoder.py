8f0d3cb688e870c23f9cd43518eaf406e4f724d1
Sandrro <antonov-as@yandex.ru>
2024-08-28 17:50:47 +0300

    fixed geocoder test

--- tests/test_geocoder.py
+++ tests/test_geocoder.py
@@ -4,20 +4,19 @@ from sloyka.src.geocoder.geocoder import Geocoder
 
 @pytest.fixture
 def sample_dataframe():
-    s_data = {
-        "text": [
-            "На биржевой 15 снова шумят!!"
-        ]
-    }
+    s_data = {'Текст комментария': {203: 'На Чайковского 63 тоже идет кап.ремонт. В квартире у пенсионеров побили стекла. Куда им обратиться?',
+    204: 'Вся улица Жуковского и Восстания заклеена рекламой! Почему не действует полиция и администрация с ЖСК-1 ?'},
+    'message_id': {203: 195, 204: 196}}
     return pd.DataFrame(s_data)
 
 
 def test_run_function(sample_dataframe):
-    instance = Geocoder(osm_id=337422, city_tags = { "place": ["state"] })
+    osm_id = 337422 # Saint Petersburg
+    geocoder = Geocoder(df=sample_dataframe, osm_id=osm_id, city_tags={'place':['state']}, text_column_name='Текст комментария')
 
-    result_df = instance.run(df=sample_dataframe, group_column=None)
+    result = geocoder.run(group_column=None)
 
-    print(result_df[['Street', 'Numbers']])
+    print(result[['Street', 'Numbers']])
 
-    assert result_df.loc[0, "Street"] == "биржевой"
-    assert result_df.loc[0, "Numbers"] == "15"
+    assert result.loc[0, "Street"] == "чайковского"
+    assert result.loc[0, "Numbers"] == "63"
